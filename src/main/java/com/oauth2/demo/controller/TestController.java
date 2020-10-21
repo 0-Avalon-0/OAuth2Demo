5e99210587ae830310ecff7b90438b2f0afd6592
Avalon <15061508266@163.com>
2020-10-21 17:35:55 +0800

    commit changes

--- src/main/java/com/oauth2/demo/controller/TestController.java
+++ src/main/java/com/oauth2/demo/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.oauth2.demo;
+package com.oauth2.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
